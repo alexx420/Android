319e00250f10ccd2ae9c74ca3342a1324e1fb15a
alexx420 <rafa.dgsl@gmail.com>
2017-03-21 15:55:32 -0600

    Agrega eventos on click en la actividad de menu
    -todos abrir actividades de bienvenida

---
+++ app/src/main/java/com/team/android/CrInfo4Activity.java
@@ -0,0 +1,17 @@
+package com.team.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by administradorcito on 21/03/2017.
+ */
+
+public class CrInfo4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
