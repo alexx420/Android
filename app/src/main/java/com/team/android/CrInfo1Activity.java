392f5289ce55b1897c6211fd1c0880f17a9681fa
alexx420 <rafa.dgsl@gmail.com>
2017-03-21 16:28:23 -0600

    Agrega todos a credito 1 y 4 activity

--- app/src/main/java/com/team/android/CrInfo1Activity.java
+++ app/src/main/java/com/team/android/CrInfo1Activity.java
@@ -2,6 +2,8 @@ package com.team.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by administradorcito on 21/03/2017.
@@ -9,9 +11,29 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CrInfo1Activity extends AppCompatActivity {
 
+    ImageButton siguiente;
+    ImageButton home;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        siguiente = (ImageButton) findViewById(R.id.btn_siguiente);
+        home = (ImageButton) findViewById(R.id.btn_menu);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO alexx420 agregar siguiente activity
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO alexx420 agregar home activity
+            }
+        });
 
     }
 }
