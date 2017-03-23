c2d21ec6b9d720922499942e0db80c8647327e34
alexx420 <rafa.dgsl@gmail.com>
2017-03-22 23:25:09 -0600

    Bug fix
    -Corrige bug en el juego 1 de credito tablero y dados

--- app/src/main/java/com/team/android/MenuActivity.java
+++ app/src/main/java/com/team/android/MenuActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
