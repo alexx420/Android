2e039b511625635dae8b880a5276e47d0eb752ef
alexx420 <rafa.dgsl@gmail.com>
2017-03-30 13:49:32 -0600

    Agrega preguntas random a juego 1

--- app/src/main/java/com/team/android/CrJuego1Activity.java
+++ app/src/main/java/com/team/android/CrJuego1Activity.java
@@ -1,5 +1,6 @@
 package com.team.android;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.lang.reflect.Field;
 
@@ -16,11 +18,12 @@ import java.lang.reflect.Field;
  */
 
 public class CrJuego1Activity extends AppCompatActivity {
-    ImageButton dado;
-    ImageButton home;
+    private ImageButton dado;
+    private ImageButton home;
     private int meta = 22;
     private int pos = 0;
     private boolean ganador = false;
+    private Pregunta anterior = null;
 
 
     @Override
@@ -98,35 +101,40 @@ public class CrJuego1Activity extends AppCompatActivity {
                     imgv_dado.setImageResource(R.drawable.img_cr_dado6);
                     break;
             }
-            int anterior = this.pos;
-            //pregunta si llega a la meta
-            if (this.pos + valorDado == this.meta) {
-                //es ganador
-                this.ganador = true;
-                this.pos += valorDado;
-            } else if (this.pos + valorDado > this.meta) {
-                //pregunta si se pasa
-                //no suma valor
-            } else {
-                this.pos += valorDado;
-            }
-            //pinta casillas
-            //opaca casilla del tiro anterior
-            int id_view = getId("imageViewC" + anterior, R.id.class);
-            ImageView imgv_casilla = (ImageView) findViewById(id_view);
-            int id_drwbl = getId("img_cr_casilla" + anterior + "_uf", R.drawable.class);
-            imgv_casilla.setImageResource(id_drwbl);
+            //muestra pregunta si es acierto avanza las casillas sino se regresa
+            muestraPregunta();
+            if (isAcierto) {
 
-            //activa casilla del tiro nuevo
-            id_view = getId("imageViewC" + this.pos, R.id.class);
-            imgv_casilla = (ImageView) findViewById(id_view);
-            id_drwbl = getId("img_cr_casilla" + this.pos, R.drawable.class);
-            imgv_casilla.setImageResource(id_drwbl);
-            //pregunta si es ganador
-            if (ganador) {
-                //si
-                // muestra mensaje
-                mensajeGanador();
+                int anterior = this.pos;
+                //pregunta si llega a la meta
+                if (this.pos + valorDado == this.meta) {
+                    //es ganador
+                    this.ganador = true;
+                    this.pos += valorDado;
+                } else if (this.pos + valorDado > this.meta) {
+                    //pregunta si se pasa
+                    //no suma valor
+                } else {
+                    this.pos += valorDado;
+                }
+                //pinta casillas
+                //opaca casilla del tiro anterior
+                int id_view = getId("imageViewC" + anterior, R.id.class);
+                ImageView imgv_casilla = (ImageView) findViewById(id_view);
+                int id_drwbl = getId("img_cr_casilla" + anterior + "_uf", R.drawable.class);
+                imgv_casilla.setImageResource(id_drwbl);
+
+                //activa casilla del tiro nuevo
+                id_view = getId("imageViewC" + this.pos, R.id.class);
+                imgv_casilla = (ImageView) findViewById(id_view);
+                id_drwbl = getId("img_cr_casilla" + this.pos, R.drawable.class);
+                imgv_casilla.setImageResource(id_drwbl);
+                //pregunta si es ganador
+                if (ganador) {
+                    //si
+                    // muestra mensaje
+                    mensajeGanador();
+                }
             }
         }
     }
@@ -135,4 +143,84 @@ public class CrJuego1Activity extends AppCompatActivity {
         Field idField = c.getDeclaredField(resourceName);
         return idField.getInt(idField);
     }
+
+    private boolean isAcierto;
+
+    private void muestraToast() {
+        if (isAcierto)
+            Toast.makeText(this, "Correcto! avanzas a la siguiente casilla", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Incorrecto, retrocedes las casillas que avanzaste", Toast.LENGTH_SHORT).show();
+    }
+
+    private void muestraPregunta() {
+        final Pregunta preguntaRandom = obtenerPreguntaRandom();
+        AlertDialog.Builder builder = new AlertDialog.Builder(CrJuego1Activity.this);
+        builder.setTitle("Pregunta");
+        builder.setMessage(preguntaRandom.getPregunta())
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (preguntaRandom.isRespuesta())
+                            isAcierto = true;
+                        else
+                            isAcierto = false;
+                        muestraToast();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (!preguntaRandom.isRespuesta())
+                            isAcierto = true;
+                        else
+                            isAcierto = false;
+                        muestraToast();
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+    private Pregunta obtenerPreguntaRandom() {
+        Pregunta pregunta;
+        int numeroRandom = (int) Math.floor(Math.random() * 9 + 1);
+        do {
+            pregunta = Pregunta.valueOf("P_" + numeroRandom);
+        } while (pregunta.equals(anterior));
+
+        anterior = pregunta;
+        return pregunta;
+    }
+
+
+}
+
+enum Pregunta {
+
+    P_1("¿Usamos el crédito cuando tenemos la necesidad o la meta de adquirir un bien cuyo valor esta fuera de nuestras Posibilidades?", true),
+    P_2("¿Sólo podemos usar el crédito a nuestro favor para comprar productos simples y cotidianos?", false),
+    P_3("¿Sólo podemos usar el crédito a nuestro favor para comprar productos con precio elevado?", false),
+    P_4("¿En la sociedad actual prácticamente no hay consumo importante sin crédito?", true),
+    P_5("¿En la sociedad actual prácticamente no hay necesidad de utilizar el crédito?", false),
+    P_6("¿El crédito nunca se puede usar a nuestro favor?", false),
+    P_7("¿El crédito es un instrumento financiero?", true),
+    P_8("¿El crédito bien utilizado se puede usar a nuestro favor?", true),
+    P_9("¿El crédito es un instrumento estadístico?", false);
+
+    private final String pregunta;
+    private final boolean respuesta;
+
+    Pregunta(String pregunta, boolean respuesta) {
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public boolean isRespuesta() {
+        return respuesta;
+    }
+
+
 }
